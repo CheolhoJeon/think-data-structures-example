cfbbf085b03f24a2e86fd7b587981f0de047e8d0
Cheolho Jeon <cheolho@edu.hanbat.ac.kr>
2021-09-10 13:28:54 +0900

    euisoo message

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.assertj:assertj-core:3.20.2")
+    implementation("org.jfree:jcommon:1.0.24")
+    implementation("org.jfree:jfreechart:1.5.3")
+    implementation("org.apache.commons:commons-math3:3.6")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
---
+++ src/main/java/com/charlie/AA.java
@@ -0,0 +1,11 @@
+package com.charlie;
+
+public class AA {
+
+
+public E get(int index) {
+    Node node = getNode(index);
+    return node.data;
+}
+
+}
